58b6a7e831f0173d44d809637e8b82f20269650f
Tara Sabella <tarasabella@gmail.com>
2018-11-22 02:38:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,15 @@ def reverse_each_word(sentence)
   sentence.split(", ")
  end 
 end 
+
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
