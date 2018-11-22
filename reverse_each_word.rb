900eb40eef3ac10a932ad6afe635b450d2f53781
Tara Sabella <tarasabella@gmail.com>
2018-11-22 02:40:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,13 +7,3 @@ def reverse_each_word(sentence)
 
 
 
-
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
-end
-
-# Without iterating:
-
-# def reverse_each_word(sentence)
-#  sentence.reverse.split.reverse.join(" ")
-# end
